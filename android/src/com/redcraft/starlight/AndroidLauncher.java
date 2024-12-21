0c7e5d5f51e40409bf883a55afb407bf93149a12
BuildTools <unconfigured@null.spigotmc.org>
2024-12-21 12:29:40 +0100

    Added Android permissions

--- android/src/com/redcraft/starlight/AndroidLauncher.java
+++ android/src/com/redcraft/starlight/AndroidLauncher.java
@@ -4,12 +4,16 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.redcraft.starlight.launcher.HomeScreen;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
+		HomeScreen.DEFAULT_FONT_SIZE = 40;
+		ApplicationHook hook = new ApplicationHook(new HomeScreen());
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new StarlightDefender(), config);
+		initialize(hook, config);
 	}
 }
